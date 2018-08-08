e79dda2db1b96d5897e0b9139fa3f70e51c822d3
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2018-08-08 15:26:20 -0700

    Added findAllByName method in CategoryJpaRepository.

--- src/main/java/ecommerce/tutorial/jpa/repositories/CategoryJpaRepository.java
+++ src/main/java/ecommerce/tutorial/jpa/repositories/CategoryJpaRepository.java
@@ -2,9 +2,11 @@ package ecommerce.tutorial.jpa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 import ecommerce.tutorial.jpa.entities.CategoryEntity;
 
 public interface CategoryJpaRepository extends JpaRepository<CategoryEntity, Long>
 {
-
+    List<CategoryEntity> findAllByName(String name);
 }
