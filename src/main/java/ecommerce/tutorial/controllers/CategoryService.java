319ca416f413404f5b973acbc7cbdb4d38d657f5
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2018-08-08 11:44:12 -0700

    Added two GetMapping in RestController for Categories to:       - get all Categories in MongoDB.       - get a specific Category by name in MongoDB.

--- src/main/java/ecommerce/tutorial/controllers/CategoryService.java
+++ src/main/java/ecommerce/tutorial/controllers/CategoryService.java
@@ -1,5 +1,58 @@
 package ecommerce.tutorial.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import ecommerce.tutorial.jpa.repositories.CategoryJpaRepository;
+import ecommerce.tutorial.jpa.repositories.ProductJpaRepository;
+import ecommerce.tutorial.jpa.repositories.SellerJpaRepository;
+import ecommerce.tutorial.mongodb.models.Category;
+import ecommerce.tutorial.mongodb.repositories.CategoryRepository;
+import ecommerce.tutorial.mongodb.repositories.ProductRepository;
+import ecommerce.tutorial.mongodb.repositories.SellerRepository;
+
+@RestController
+@RequestMapping(path = "/category")
 public class CategoryService
 {
+    @Autowired
+    private CategoryRepository _categoryMongoRepository;
+    @Autowired
+    private ProductRepository _productMongoReposirory;
+    @Autowired
+    private SellerRepository _sellerMongoRepository;
+
+    @Autowired
+    private CategoryJpaRepository _categoryJpaRepository;
+    @Autowired
+    ProductJpaRepository _productJpaRepository;
+    @Autowired
+    SellerJpaRepository _sellerJpaRepository;
+
+    @GetMapping(path = "/mongo")
+    public ResponseEntity<Category> getCategoryFromMongoDB(@RequestParam(value = "name") String name)
+    {
+
+        Category categoryMongo = _categoryMongoRepository.findByName(name);
+        if (categoryMongo != null)
+        {
+            return new ResponseEntity<Category>(categoryMongo, HttpStatus.OK);
+
+        }
+        System.out.println("There isn't any Category in Mongo database with this name!");
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping(path = "/all/mongo")
+    public List<Category> getAllCategoriesFromMongoDB()
+    {
+        return _categoryMongoRepository.findAll();
+    }
 }
