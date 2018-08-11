2b605fca25d18462df22c394dd4fc1f067a6b308
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2018-08-10 18:24:44 -0700

    Added a new Override for findById in CrudRepository.

--- src/main/java/ecommerce/tutorial/mongodb/repositories/CategoryRepository.java
+++ src/main/java/ecommerce/tutorial/mongodb/repositories/CategoryRepository.java
@@ -2,9 +2,14 @@ package ecommerce.tutorial.mongodb.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 import ecommerce.tutorial.mongodb.models.Category;
 
-public interface CategoryRepository extends MongoRepository<Category, Long>
+public interface CategoryRepository extends MongoRepository<Category, String>
 {
     Category findByName(String categoryName);
+
+    @Override
+    Optional<Category> findById(String id);
 }
