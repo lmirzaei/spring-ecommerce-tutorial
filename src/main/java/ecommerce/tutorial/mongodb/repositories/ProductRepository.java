09b8344e36f2cb8a72912fbdb87f2c813006a1e4
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2018-08-24 15:55:59 -0700

    Added findByName and findById in ProductRepository for getting products in MongoDB via Name and string-type id!

--- src/main/java/ecommerce/tutorial/mongodb/repositories/ProductRepository.java
+++ src/main/java/ecommerce/tutorial/mongodb/repositories/ProductRepository.java
@@ -2,9 +2,15 @@ package ecommerce.tutorial.mongodb.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 import ecommerce.tutorial.mongodb.models.Product;
 
-public interface ProductRepository extends MongoRepository<Product, Long>
+public interface ProductRepository extends MongoRepository<Product, String>
 {
 
+    Product findByName(String name);
+
+    @Override
+    Optional<Product> findById(String id);
 }
