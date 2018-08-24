c8b4708939d9e4421706ad7601e2eaed1fef9baf
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2018-08-24 15:51:41 -0700

    Override the findById method in CrudRepository to accept id with String type.

--- src/main/java/ecommerce/tutorial/mongodb/repositories/SellerRepository.java
+++ src/main/java/ecommerce/tutorial/mongodb/repositories/SellerRepository.java
@@ -4,11 +4,15 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 import ecommerce.tutorial.mongodb.models.Seller;
 
-public interface SellerRepository extends MongoRepository<Seller, Long>
+public interface SellerRepository extends MongoRepository<Seller, String>
 {
     @Query("{'profile.firstName': ?0}")
     List<Seller> findByFirstName(String firstName);
+
+    @Override
+    Optional<Seller> findById(String s);
 }
