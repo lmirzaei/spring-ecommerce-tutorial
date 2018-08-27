369e6b9317d241821bd19e06be4fe29a417266c5
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2018-08-26 17:36:42 -0700

    Added EmbeddedCategory class because I need to save the id and name of a Category in fallIntoCategories of Product but I don't want to save productsOfCategory to be saved in product documents. So, I created a new class which has the id and name of each category and I use this class to save embedded properties in the Product documents.

---
+++ src/main/java/ecommerce/tutorial/mongodb/models/EmbeddedCategory.java
@@ -0,0 +1,38 @@
+package ecommerce.tutorial.mongodb.models;
+
+public class EmbeddedCategory
+{
+    private String id;
+
+    private String name;
+
+    public EmbeddedCategory()
+    {
+    }
+
+    public EmbeddedCategory(String id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+}
