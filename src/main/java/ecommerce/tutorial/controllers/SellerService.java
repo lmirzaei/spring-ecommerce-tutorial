ba4c0f53f98aaf7f5e4c68c94cc9ccbe73762e44
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2018-08-27 19:43:01 -0700

    Added implementation for service of seller.

--- src/main/java/ecommerce/tutorial/controllers/SellerService.java
+++ src/main/java/ecommerce/tutorial/controllers/SellerService.java
@@ -1,5 +1,168 @@
 package ecommerce.tutorial.controllers;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.result.UpdateResult;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+import javax.validation.Valid;
+
+import ecommerce.tutorial.jpa.entities.ProfileEntity;
+import ecommerce.tutorial.jpa.entities.SellerEntity;
+import ecommerce.tutorial.jpa.repositories.SellerJpaRepository;
+import ecommerce.tutorial.mongodb.models.Profile;
+import ecommerce.tutorial.mongodb.models.Seller;
+import ecommerce.tutorial.mongodb.repositories.SellerRepository;
+
+@RestController
+@RequestMapping(path = "/seller")
 public class SellerService
 {
+    private MongoOperations _mongoOperation = new MongoTemplate(new MongoClient(), "local");//TODO: How to know databaseName?
+    @Autowired
+    private SellerJpaRepository _sellerJpaRepository;
+    @Autowired
+    private SellerRepository _sellerMongoRepository;
+
+
+    //--------------------------------------Retrieve (a) Sellers------------------------------------------------
+    @GetMapping(path = "/mongo")
+    public ResponseEntity<?> getSellersFromMongoDB(@RequestParam(value = "firstName") String firstName)
+    {
+        List<Seller> sellers = _sellerMongoRepository.findByFirstName(firstName);
+        if (sellers.size() > 0)
+        {
+            System.out.println("There are " + sellers.size() + " sellers with first name " + firstName + " in MongoDB database.");
+            return new ResponseEntity<>(sellers, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("There isn't any seller with this name in MongoDB.", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(path = "/all/mongo")
+    public List<Seller> getAllSellersFromMongoDB()
+    {
+        return _sellerMongoRepository.findAll();
+    }
+
+    @GetMapping(path = "/mysql")
+    public ResponseEntity<?> getSellerFromMysql(@RequestParam(value = "id") long id)
+    {
+        try
+        {
+            SellerEntity seller = _sellerJpaRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+            System.out.println("The seller with id " + id + " = " + seller.toString());
+            return new ResponseEntity<>(seller, HttpStatus.OK);
+        }
+        catch (EntityNotFoundException e)
+        {
+            return new ResponseEntity<>("There isn't any seller with this name in MySQL.", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping(path = "/all/mysql")
+    public List<SellerEntity> getAllSellersFromMysql()
+    {
+        return _sellerJpaRepository.findAll();
+    }
+
+    //--------------------------------------Create a Seller-----------------------------------------------------
+    @PostMapping(path = "/mongo")
+    public ResponseEntity<Seller> addNewSellerInMongoDB(@Valid @RequestBody Seller seller)
+    {
+        Profile profile = new Profile(seller.getProfile().getFirstName(), seller.getProfile().getLastName(), seller.getProfile().getGender());
+        Seller sellerMongoDB = new Seller(seller.getAccountId(), profile);
+        sellerMongoDB = _sellerMongoRepository.save(sellerMongoDB);
+        return new ResponseEntity<>(sellerMongoDB, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/mysql")
+    public ResponseEntity<SellerEntity> addNewSellerInMysql(@Valid @RequestBody SellerEntity seller)
+    {
+        SellerEntity sellerEntity = new SellerEntity(seller.getAccountId());
+        ProfileEntity profile = new ProfileEntity(sellerEntity, seller.getProfile().getFirstName(), seller.getProfile().getLastName(), seller.getProfile().getGender());
+        sellerEntity.setProfile(profile);
+        sellerEntity.getProfile().setWebsite(seller.getProfile().getWebsite());
+        sellerEntity.getProfile().setAddress(seller.getProfile().getAddress());
+        sellerEntity.getProfile().setEmailAddress(seller.getProfile().getEmailAddress());
+        sellerEntity.getProfile().setBirthday(seller.getProfile().getBirthday());
+        sellerEntity = _sellerJpaRepository.save(sellerEntity);
+        return new ResponseEntity<>(sellerEntity, HttpStatus.OK);
+    }
+
+    //--------------------------------------Update a Seller-----------------------------------------------------
+    @PutMapping(path = "/mongo")
+    public ResponseEntity<String> updateSellerInMongoDB(@Valid @RequestBody Seller seller)
+    {
+        try
+        {
+            Seller sellerInDatabase = _sellerMongoRepository.findById(seller.getId()).orElseThrow(EntityNotFoundException::new);
+            Update update = new Update();
+            update.set("accountId", seller.getAccountId());
+            update.set("profile.firstName", seller.getProfile().getFirstName());
+            update.set("profile.lastName", seller.getProfile().getLastName());
+            update.set("profile.website", seller.getProfile().getWebsite());
+            update.set("profile.birthday", seller.getProfile().getBirthday());
+            update.set("profile.address", seller.getProfile().getAddress());
+            update.set("profile.emailAddress", seller.getProfile().getEmailAddress());
+            update.set("profile.gender", seller.getProfile().getGender());
+
+            Query query = new Query(Criteria.where("_id").is(seller.getId()));
+            UpdateResult updateResult = _mongoOperation.updateFirst(query, update, Seller.class);
+            if (updateResult.getModifiedCount() == 1)
+            {
+                sellerInDatabase = _sellerMongoRepository.findById(seller.getId()).orElseThrow(EntityNotFoundException::new);
+                System.out.println("__________________________________________________________________");
+                System.out.println("The document of " + sellerInDatabase.toString() + " updated");
+                return new ResponseEntity<>("The seller updated", HttpStatus.OK);
+            }
+            else
+            {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        catch (EntityNotFoundException e)
+        {
+            return new ResponseEntity<>("This seller doesn't exists in MongoDB.", HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping(path = "/mysql")
+    public ResponseEntity<String> updateSellerInMysql(@Valid @RequestBody SellerEntity seller)
+    {
+        SellerEntity sellerEntity = _sellerJpaRepository.findById(seller.getId()).orElse(null);
+        if (sellerEntity == null)
+        {
+            return new ResponseEntity<>("This seller doesn't exists in MySQL.", HttpStatus.NOT_FOUND);
+        }
+        sellerEntity.setAccountId(seller.getAccountId());
+        sellerEntity.getProfile().setFirstName(seller.getProfile().getFirstName());
+        sellerEntity.getProfile().setLastName(seller.getProfile().getLastName());
+        sellerEntity.getProfile().setWebsite(seller.getProfile().getWebsite());
+        sellerEntity.getProfile().setBirthday(seller.getProfile().getBirthday());
+        sellerEntity.getProfile().setAddress(seller.getProfile().getAddress());
+        sellerEntity.getProfile().setEmailAddress(seller.getProfile().getEmailAddress());
+        sellerEntity.getProfile().setGender(seller.getProfile().getGender());
+        sellerEntity = _sellerJpaRepository.save(sellerEntity);
+        System.out.println("__________________________________________________________________");
+        System.out.println("The row of " + sellerEntity.toString() + " updated");
+        return new ResponseEntity<>("The seller updated", HttpStatus.OK);
+    }
 }
