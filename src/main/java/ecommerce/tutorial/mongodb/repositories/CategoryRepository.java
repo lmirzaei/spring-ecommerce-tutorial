6ff11c70f7cfa3b1a841392f60b63700f4e5149b
Leila Mirzaei <41973606+lmirzaei@users.noreply.github.com>
2018-09-09 18:47:20 -0700

    Deleted override for findById method!

--- src/main/java/ecommerce/tutorial/mongodb/repositories/CategoryRepository.java
+++ src/main/java/ecommerce/tutorial/mongodb/repositories/CategoryRepository.java
@@ -2,14 +2,9 @@ package ecommerce.tutorial.mongodb.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
 import ecommerce.tutorial.mongodb.models.Category;
 
 public interface CategoryRepository extends MongoRepository<Category, String>
 {
     Category findByName(String categoryName);
-
-    @Override
-    Optional<Category> findById(String id);
 }
